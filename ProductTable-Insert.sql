8247080542d0221eeb9bd9be11e9500df4c534b7
Quintessa Anderson <quintessaanderson@Quintessas-MacBook-Pro.local>
2018-03-02 22:57:35 -0600

    deleted duplicate files

--- ProductTable-Insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO products(product_name, department_name, customer_price)
-VALUES 
-("television", "electronics", 800.00, 50),
-("computers", "electronics", 1000, 100),
-("office-chairs", "office", 80, 20)
